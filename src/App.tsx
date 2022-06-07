6f039c2f6555a12e98c671e18475c9adfb11ff9a
kkvzx <64148572+kkvzx@users.noreply.github.com>
2022-06-07 16:03:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,10 @@ function App() {
 export default App;
 
 // TODO
+// Add Darkmode
 // Track the number of rolls
 // Track the time it took to win
 // Save your best time to localStorage
+
+// NEXT PROJECT - QUIZZICAL TRIVIA
+// 11:45
