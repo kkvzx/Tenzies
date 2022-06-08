c8bbc701982b8edca665305eab27e6c71b23bdcd
kkvzx <64148572+kkvzx@users.noreply.github.com>
2022-06-08 15:24:40 +0200

    summary subpage completed
    in addition: reset counter

--- src/componenets/Header.tsx
+++ src/componenets/Header.tsx
@@ -1,21 +1,9 @@
 import React from "react";
 
-interface headerProps {
-  toggle: () => void;
-  darkMode: boolean;
-}
 // Text content
-export const Header = (props: headerProps) => {
+export const Header = () => {
   return (
     <div className="headerWrapper">
-      <label className="switch">
-        <input
-          type="checkbox"
-          checked={props.darkMode}
-          onChange={props.toggle}
-        />
-        <span className="slider round"></span>
-      </label>
       <h1>Tenzies</h1>
       <p>Roll until all dice are the same. Click</p>
       <p> each die to freeze it at its current value </p>
